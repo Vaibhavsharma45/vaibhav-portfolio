16b19b7153f9c3e4206d285401a911db773139d3
Vaibhavsharma45 <vaibhavsharma95124v@gmail.com>
2026-02-06 13:05:45 +0530

    Updated portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,15 +6,16 @@ export default function Contact() {
       <p className="mt-4">
         Open to internships, freelance, and full-time opportunities.
       </p>
+      <p className="mt-2 text-lg">
+  I’m actively looking for internships and entry-level software roles.
+</p>
+
       <a
         href="mailto:yourmail@gmail.com"
         className="inline-block mt-6 px-6 py-3 bg-white text-primary rounded-lg"
       >
         Contact Me
       </a>
-      <p className="mt-2 text-lg">
-  I’m actively looking for internships and entry-level software roles.
-</p>
     </section>
   );
 }
